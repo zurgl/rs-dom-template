f2b2cc061b6498c5e5d0dbf9e247e2404df1e4fa
zurgl <zurgly@gmail.com>
2023-06-01 20:18:18 +0200

    improvement

--- src/logo/mod.rs
+++ src/logo/mod.rs
@@ -1,2 +1,14 @@
 pub mod brand;
 pub mod brand_with_text;
+
+use futures_signals::signal::{Signal, SignalExt};
+use std::sync::Arc;
+
+use crate::{app::App, component::theme::Theme};
+
+pub fn update_fill_color(app: Arc<App>) -> impl Signal<Item = &'static str> {
+    app.theme.signal().map(|theme| match theme {
+        Theme::Light => "#b91c1c",
+        Theme::Dark => "#22c55e",
+    })
+}
